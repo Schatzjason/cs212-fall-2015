a28928c0f4301a25b167b161d416745c41c1db61
schatzjason <schatz@gmail.com>
2015-10-15 21:04:49 -0700

    complete Galazy from class

--- Galaxy/Galaxy/ViewController.swift
+++ Galaxy/Galaxy/ViewController.swift
@@ -11,15 +11,88 @@ import UIKit
 class ViewController: UIViewController {
     
     let GalaxyURL: String = "https://s3.amazonaws.com/policymic-images/oidbjjmfalmyrfd1ohj8sjezcqu7kwxg7bpil5pddk8ky09amyszdhenheurzrj2.jpg"
-
+    
+//    let GalaxyURL: String = "http://icons.iconarchive.com/icons/aha-soft/space/256/Galaxy-icon.png"
+    
+    var request: NSURLRequest!
+    var session: NSURLSession!
+    var task: NSURLSessionDataTask?
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    var downloading : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.hidden = true
+        progressView.hidden = true
+        
+        self.request = NSURLRequest(URL: NSURL(string: GalaxyURL)!)
+        self.session = NSURLSession.sharedSession()
+    }
+    
+    @IBAction func startOrCancel(sender: UIButton) {
+        
+        downloading = !downloading
+        
+        if downloading {
+            button.setTitle("Cancel", forState: UIControlState.Normal)
+            activityIndicator.hidden = false
+            activityIndicator.startAnimating()
+            progressView.hidden = false
+            startDownload()
+        }
+            
+        else {
+            button.setTitle("Start", forState: UIControlState.Normal)
+            activityIndicator.hidden = true
+            activityIndicator.stopAnimating()
+            progressView.hidden = true
+            
+            assert(task != nil, "Task should not be nil")
+            
+            task!.cancel()
+            task = nil
+        }
+        
+    }
+    
+    func startDownload() {
+        task = session.dataTaskWithRequest(request, completionHandler: {
+            data, response, error in
+            
+            if let error = error {
+                println(error)
+            } else {
+                
+                let image = UIImage(data: data)
+                
+                // This should be sent back to the MAIN Thread
+                // Grand Central Dispatch code
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.downloading = false
+                    self.imageView.image = image
+                    
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.hidden = true
+                    self.progressView.hidden = true
+                    self.button.setTitle("Start", forState: UIControlState.Normal)
+                })
+
+                println("Download complete")
+            }
+        })
+        task!.resume()
     }
 
 }
+
+
+
+
+
+
 
